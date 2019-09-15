5ad90dc6d688e3f85d48fdc1bc4fd4ce3bc181e2
mahdi zareei <mahdizareei.hmd@gmail.com>
2019-09-15 11:06:41 +0430

    cleanup main activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.myapplication;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
