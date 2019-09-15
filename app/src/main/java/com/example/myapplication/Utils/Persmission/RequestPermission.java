e4fe2f02f6d793b8a9dffbcfdb13dd8b9e7d4752
mahdi zareei <mahdizareei.hmd@gmail.com>
2019-09-15 16:14:06 +0430

    request permission class

--- app/src/main/java/com/example/myapplication/Utils/Persmission/RequestPermission.java
+++ app/src/main/java/com/example/myapplication/Utils/Persmission/RequestPermission.java
@@ -1,18 +1,14 @@
 package com.example.myapplication.Utils.Persmission;
 
-import android.app.Application;
 import android.content.pm.PackageManager;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.example.myapplication.Utils.BaseActivity;
-
 public class RequestPermission {
-    AppCompatActivity appCompatActivity;
+    private AppCompatActivity appCompatActivity;
 
     public RequestPermission(AppCompatActivity appCompatActivity) {
         this.appCompatActivity = appCompatActivity;
@@ -21,8 +17,8 @@ public class RequestPermission {
     public void checkPermission(@NonNull String[] permission, int requestCode) {
         StringBuilder a = new StringBuilder();
         a.append("1");
-        for (int i = 0; i < permission.length; i++)
-            if (ContextCompat.checkSelfPermission(appCompatActivity, permission[i]) != PackageManager.PERMISSION_GRANTED) {
+        for (String s : permission)
+            if (ContextCompat.checkSelfPermission(appCompatActivity, s) != PackageManager.PERMISSION_GRANTED) {
                 a.append("0");
             }
         if (a.toString().contains("0")) {
